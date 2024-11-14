44b27731bed5bf5c212b7b54cb1a8e9fc579c422
Malik-Adil563 <madil4329@gmail.com>
2024-11-14 19:32:44 +0500

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:8000/register', { name, email, password });
+      const response = await axios.post('https://ecommerce-ar-backend.vercel.app/register', { name, email, password });
       console.log('Response:', response);
       if (response.data && response.data.token) {
         navigate('/login');
