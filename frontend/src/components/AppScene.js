bf51beba534c43299dadf309cb5f74fc0de4eda5
Malik-Adil563 <madil4329@gmail.com>
2024-12-27 15:32:10 +0500

    Update AppScene.js

--- frontend/src/components/AppScene.js
+++ frontend/src/components/AppScene.js
@@ -51,8 +51,8 @@ const AppScene = () => {
       (gltf) => {
         model = gltf.scene;
         model.scale.set(0.02, 0.02, 0.02); // Adjusted scale
-        
-        model.position.set(0, 1, -4); // Adjusted position (higher and forward)
+        model.rotation.x = Math.PI / 4; // Keep it upright
+        model.position.set(0, 1, -3); // Adjusted position (higher and forward)
         scene.add(model);
       },
       undefined,
@@ -79,7 +79,7 @@ const AppScene = () => {
       const originalRotation = model.rotation.clone();
 
       model.quaternion.setFromRotationMatrix(controller.matrixWorld);
-      
+      model.rotation.x = originalRotation.x;
       model.rotation.y = originalRotation.y;
       model.rotation.z = originalRotation.z;
       model.scale.copy(originalScale);
